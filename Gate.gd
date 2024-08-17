a71587e6c150a1f9dae00873ef540eb8e19bfce2
meepmeerp <dylgoh@gmail.com>
2024-08-17 14:39:33 +0800

    Added demo level environment

---
+++ Gate.gd
@@ -0,0 +1,16 @@
+extends AnimatableBody3D
+
+var open:bool = false
+
+
+func _on_sensor_activate() -> void:
+	if(!open):
+		print("detected!")
+		print(global_position)
+		#translate(Vector3i(0,10,0))
+		var tween = get_tree().create_tween()
+		tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+		tween.tween_property(self,"global_position",global_position+Vector3(0,10,0),1)
+		open = true
+	
+	
