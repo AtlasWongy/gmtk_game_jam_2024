e03a829572c5a84e8b98ab1f17ebd19d9dc1cc1d
AtlasWongy <atlasyjyj@gmail.com>
2024-08-17 16:12:52 +0800

    Fixed the movement for player

--- characters/player/red_box/red_box.gd
+++ characters/player/red_box/red_box.gd
@@ -1,44 +1,27 @@
 extends CharacterBody3D
+class_name Player
+
+@export var movement_stats: MovementStats
 
 var movement_direction: Vector3
-var speed: float = 5.0
-var acceleration: float = 1.0
-var is_jumping: bool = false
-var fall_gravity: int = 45
-var jump_gravity: float = fall_gravity
-var jump_height: float = 4.0
-var apex_duration: float = 0.5
 
 func _input(event: InputEvent) -> void:
-	movement_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	
-	if event.is_action_pressed("jump") and !is_jumping:
-		is_jumping = true
+	if event.is_action("movement"):
+		movement_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 		
-func _physics_process(delta: float) -> void:
-	if !is_on_floor():
-		if velocity.y >= 0:
-			velocity.y -= jump_gravity * delta
+		if is_movement_ongoing():
+			SignalBus.set_movement.emit(movement_stats)
 		else:
-			velocity.y -= fall_gravity * delta
-	
-	if is_jumping and is_on_floor():
-		jump()
-		is_jumping = false
+			SignalBus.set_movement.emit(null)
 			
+	if event.is_action_pressed("jump") and is_on_floor():
+		SignalBus.pressed_jump.emit()
+			
+func _physics_process(_delta: float) -> void:
 	if is_movement_ongoing():
-		velocity.x = speed * movement_direction.normalized().x
-	else:
-		velocity.x = 0 * movement_direction.normalized().x
-		
-	velocity = velocity.lerp(velocity, acceleration * delta)
-	move_and_slide()
-		
+		SignalBus.set_direction.emit(movement_direction)
+
 func is_movement_ongoing() -> bool:
 	return abs(movement_direction.x) > 0
-	
-func jump():
-	velocity.y = 2 * jump_height / apex_duration
-	jump_gravity = velocity.y / apex_duration
 	
 	
--- singletons/signal_bus.gd
+++ singletons/signal_bus.gd
@@ -1,1 +1,5 @@
 extends Node
+
+signal set_movement(_movement_stats: MovementStats)
+signal set_direction(_movement_direction: Vector3)
+signal pressed_jump()
