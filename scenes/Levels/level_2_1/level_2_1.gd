c8a769bfcd9e002ec0da563c74eed5b6dd34eb77
meepmeerp <dylgoh@gmail.com>
2024-08-19 23:11:29 +0800

    Tentatively working commit

--- scenes/Levels/level_2_1/level_2_1.gd
+++ scenes/Levels/level_2_1/level_2_1.gd
@@ -1,2 +1,63 @@
 extends Node3D
 #idea: block lasers? idk man
+
+@onready var gate = $GrayGate
+@onready var laser = $Laser2
+@onready var platform = $Platform10
+@onready var flag_platform = $Platform4
+
+var gate_up: bool = false
+var laser_rotated:bool = false
+var platform_up:bool = false
+
+var flag_opened:bool = false
+
+const gate_down_coords = Vector3(-8.5,12,0)
+const gate_up_coords = Vector3(-8.5,14,0)
+
+const platform_up_coords = Vector3(-2.15,7.5,0)
+const platform_down_coords = Vector3(-2.15,6.5,0)
+
+func _physics_process(delta:float):
+	if(GameManager.level_id != 0):
+		return
+	
+	if(gate_up):
+		gate.position = gate.position.move_toward(gate_up_coords,delta*5)
+	else:
+		gate.position = gate.position.move_toward(gate_down_coords,delta*5)
+	
+	if(laser_rotated):
+		if(laser.rotation_degrees.x <= 0.1 and laser.rotation_degrees.x > -90):
+			laser.rotation_degrees += Vector3(max(-90-laser.rotation_degrees.x,-5),0,0)
+
+	elif !laser_rotated:
+		if laser.rotation_degrees.x < 0.1 and laser.rotation_degrees.x >= -90:
+			laser.rotation_degrees += Vector3(min(-laser.rotation_degrees.x,5),0,0)
+	
+	if(platform_up):
+			platform.position = platform.position.move_toward(platform_up_coords,delta*5)
+	else:
+		platform.position = platform.position.move_toward(platform_down_coords,delta*5)
+	
+	if(flag_opened):
+		flag_platform.position = flag_platform.position.move_toward(Vector3(5,6.5,0),delta*5)
+
+func _on_laser_blocked(id: int) -> void:
+	if(id==0):
+		gate_up = true
+	elif(id==1):
+		laser_rotated = true
+	elif(id==2):
+		platform_up = true
+	elif(id==3):
+		if(laser_rotated):
+			flag_opened = true
+
+func _on_laser_not_blocked(id: int) -> void:
+	if(id==0):
+		gate_up = false
+	elif(id==1):
+		laser_rotated = false
+	elif(id==2):
+		platform_up = false
---
+++ scenes/stage.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.register_level.emit(self)
+	SignalBus.spawn_cube.emit(GameManager.CurrentBox.RED_BOX, GameManager.level_id)
+	SignalBus.spawn_cube.emit(GameManager.CurrentBox.BLUE_BOX, GameManager.level_id)
