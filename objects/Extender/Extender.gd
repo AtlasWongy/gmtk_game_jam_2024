0331cb70c11c5127728bad87589bc08e9f955416
Shaw Kee <shawkee0511@gmail.com>
2024-08-20 10:40:00 +0800

    change extender expansion and shrink logic

--- objects/Extender/Extender.gd
+++ objects/Extender/Extender.gd
@@ -13,8 +13,12 @@ var level_completed: bool = false
 
 var item_placed
 
+var orignal_mesh_position
+var original_collision_position
+
 func _ready():
-	pass
+	orignal_mesh_position = extenderMesh.get_position()
+	original_collision_position = collisionShape.get_position()
 	#sensor.connect(self, "_on_sensor_activate", [self])
 	#sensor.connect(self, "_on_sensor_deactivate", [self])
 	
@@ -25,10 +29,14 @@ func _on_sensor_activate(body) -> void:
 		var tween = get_tree().create_tween()
 		tween.set_parallel()
 		tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-		tween.tween_property(extenderMesh, "position", Vector3(x_offset, 0, 0), 1).as_relative().from_current()
-		tween.tween_property(collisionShape, "position", Vector3(x_offset, 0, 0), 1).as_relative().from_current()
-		tween.tween_property(extenderMesh.mesh, "size", Vector3(x_size, 0, 0), 1).as_relative().from_current()
-		tween.tween_property(collisionShape.shape, "size", Vector3(x_size, 0, 0), 1).as_relative().from_current()
+		tween.tween_property(extenderMesh, "position", Vector3(orignal_mesh_position.x + x_offset, 0, 0), 1)
+		tween.tween_property(collisionShape, "position", Vector3(original_collision_position.x + x_offset, 0, 0), 1)
+		tween.tween_property(extenderMesh.mesh, "size", Vector3(x_size, 1, 5), 1)
+		tween.tween_property(collisionShape.shape, "size", Vector3(x_size, 1, 5), 1)
+		#tween.tween_property(extenderMesh, "position", Vector3(x_offset, 0, 0), 1).as_relative().from_current()
+		#tween.tween_property(collisionShape, "position", Vector3(x_offset, 0, 0), 1).as_relative().from_current()
+		#tween.tween_property(extenderMesh.mesh, "size", Vector3(x_size, 0, 0), 1).as_relative().from_current()
+		#tween.tween_property(collisionShape.shape, "size", Vector3(x_size, 0, 0), 1).as_relative().from_current()
 		extend = true
 		shrink = false
 		
@@ -45,10 +53,14 @@ func _on_sensor_deactivate(body) -> void:
 		var tween = get_tree().create_tween()
 		tween.set_parallel()
 		tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-		tween.tween_property(extenderMesh, "position", Vector3(-x_offset, 0, 0), 1).as_relative().from_current()
-		tween.tween_property(collisionShape, "position", Vector3(-x_offset, 0, 0), 1).as_relative().from_current()
-		tween.tween_property(extenderMesh.mesh, "size", Vector3(-x_size, 0, 0), 1).as_relative().from_current()
-		tween.tween_property(collisionShape.shape, "size", Vector3(-x_size, 0, 0), 1).as_relative().from_current()
+		tween.tween_property(extenderMesh, "position", orignal_mesh_position, 1)
+		tween.tween_property(collisionShape, "position", original_collision_position, 1)
+		tween.tween_property(extenderMesh.mesh, "size", Vector3(3, 1, 5), 1)
+		tween.tween_property(collisionShape.shape, "size", Vector3(3, 1, 5), 1)
+		#tween.tween_property(extenderMesh, "position", Vector3(-x_offset, 0, 0), 1).as_relative().from_current()
+		#tween.tween_property(collisionShape, "position", Vector3(-x_offset, 0, 0), 1).as_relative().from_current()
+		#tween.tween_property(extenderMesh.mesh, "size", Vector3(-x_size, 0, 0), 1).as_relative().from_current()
+		#tween.tween_property(collisionShape.shape, "size", Vector3(-x_size, 0, 0), 1).as_relative().from_current()
 		shrink = true
 		extend = false
 	
